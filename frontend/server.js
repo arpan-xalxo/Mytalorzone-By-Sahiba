9194554a0d66da69852c3f482980e5c5548ad5bc
Arpan xalxo <arpan22bcs78@iiitkottayam.ac.in>
2024-12-23 21:11:20 +0530

    Add vercel.json

--- frontend/server.js
+++ frontend/server.js
@@ -4,23 +4,18 @@ const path = require('path');
 const app = express();
 const PORT = 5173;
 
-// Serve static files from the public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend', 'public')));
 
-// Serve JSON files and other specific routes
 app.get('/data.json', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'data.json'));
 });
 
-// Fallback for single-page apps (serve index.html for other routes)
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+
 
 app.get('*', (req, res, next) => {
   const ext = path.extname(req.url);
   if (!ext) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'public', 'index.html'));
   } else {
     next();
   }
@@ -29,3 +24,5 @@ app.get('*', (req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Frontend is running at http://localhost:${PORT}`);
 });
+
+module.exports = app;
